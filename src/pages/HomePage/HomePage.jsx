e20df93efdc8c4c1075b7476d214186534003a53
alina <alinkash2811@gmail.com>
2025-10-23 12:31:36 +0200

    start working on home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,51 @@
 import css from "./HomePage.module.css";
-import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
+import { useNavigate } from "react-router-dom";
+import Icon from "../../components/Icon/Icon";
 
 function HomePage() {
+  const navigate = useNavigate();
+
   return (
     <div className={css.container}>
-      <h1>Home Page</h1>
+      <div className={css.textBlock}>
+        <h1 className={css.title}>
+          The road to the <span className={css.accent}>depths</span> of the
+          human soul
+        </h1>
+        <p className={css.text}>
+          We help you to reveal your potential, overcome challenges and find a
+          guide in your own life with the help of our experienced psychologists.
+        </p>
+        <button
+          className={css.button}
+          onClick={() => navigate("/psychologists")}
+        >
+          Get Started <span className={css.arrow}>↗</span>
+        </button>
+      </div>
+
+      <div className={css.imageBlock}>
+        <img
+          src="/src/assets/hero.jpg"
+          className={css.image}
+          alt="Psychologist"
+        />
+        <div className={css.icon1}>
+          <div className={css.checkWrap}>
+            <Icon className={css.check} id="check" size={30} />
+          </div>
+          <div className={css.textWrap}>
+            <p className={css.checkText}>Experienced psychologists</p>
+            <p className={css.checkNumb}>15,000</p>
+          </div>
+        </div>
+        <div className={css.icon2}>
+          <Icon className={css.question} id="question" widht={10} height={17} />
+        </div>
+        <div className={css.icon3}>
+          <Icon className={css.users} id="users" size={19.84} />
+        </div>
+      </div>
     </div>
   );
 }
